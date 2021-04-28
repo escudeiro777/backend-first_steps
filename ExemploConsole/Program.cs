ad184f2f3e278ed68b5afeceea28f25dc7ec7a35
Nayara Nogueira Escudeiro do Nascimento <nanaescudeiro2015@gmail.com>
2021-04-28 17:20:24 -0300

    primeiros passos de back

---
+++ ExemploConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ExemploConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //var estadoDoSemaforo = Qual a cor do semáforo? 
+            // int exemplo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qual é seu nome?");
+            string nomeDaPessoa = Console.ReadLine();
+
+            Console.WriteLine("Olá " + nomeDaPessoa + "!!!");
+            
+            Console.WriteLine("Qual é o primeiro número?: ");
+            int primeiroNumero = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Digite o segundo número: ");
+            int segundoNumero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual é o terceiro número?: ");
+            int terceiroNumero = int.Parse(Console.ReadLine());
+
+            int soma = primeiroNumero + segundoNumero + terceiroNumero;
+            int divisao = soma / 3;
+            Console.WriteLine("Sua média foi: " + divisao );
+
+
+
+        }
+    }
+}

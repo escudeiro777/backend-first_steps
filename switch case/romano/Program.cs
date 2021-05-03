e148a46fbe033237d8994eaaf61c5a685f22fbb4
Nayara Nogueira Escudeiro do Nascimento <nanaescudeiro2015@gmail.com>
2021-05-03 16:07:16 -0300

    organização das pastas e exercícios dados em aula (03/05) completos!

---
+++ switch case/romano/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace romano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira seu algarismo romano aqui: ");
+            string romano = Console.ReadLine() .ToLower();
+
+            switch (romano)
+            {
+                case "i":
+                Console.WriteLine("I equivale a 1 em indo arábico");
+                    break;
+
+                case "ii":
+                Console.WriteLine("II equivale a 2 em indo arábico");
+                    break;
+
+                case "iii":
+                Console.WriteLine("III equivale a 3 em indo arábico");
+                    break;
+
+                case "iv":
+                Console.WriteLine("IV equivale a 4 em indo arábico");
+                    break;
+
+                case "v":
+                Console.WriteLine("V equivale a 5 em indo arábico");
+                    break;
+
+                case "vi":
+                Console.WriteLine("VI equivale a 6 em indo arábico");
+                    break;
+
+                case "vii":
+                Console.WriteLine("VII equivale a 7 em indo arábico");
+                    break;
+
+                case "viii":
+                Console.WriteLine("VIII equivale a 8 em indo arábico");
+                    break;
+
+                case "ix":
+                Console.WriteLine("IX equivale a 9 em indo arábico");
+                    break;
+
+                case "x":
+                Console.WriteLine("X equivale a 10 em indo arábico");
+                    break;
+
+                default:
+                Console.WriteLine("Escreva um número válido");
+                    break;
+            }
+            
+            
+        }
+    }
+}
---
+++ switch case/vogal/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ExemploSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira seu nome");
+            string nome =  Console.ReadLine().ToLower();
+
+            string letra = nome.Substring(0,1);
+
+        
+
+            switch (letra)
+            {
+                case "a":
+                
+                    Console.WriteLine("Seu nome começa com a vogal " + letra + " igual a Abelha");
+                    break;
+
+                case "e":
+                Console.WriteLine("Seu nome começa com a vogal " + letra + " igual a Elefante");
+                    break;
+
+                case "i":
+                Console.WriteLine("Seu nome começa com a vogal " + letra + " igual a Igreja");
+                    break;
+
+                case "o":
+                Console.WriteLine("Seu nome começa com a vogal " + letra + " igual a Ovo");
+                    break;
+
+                case "u":
+                Console.WriteLine("Seu nome começa com a vogal " + letra + " igual a Urubu");
+                    break;
+
+                default:
+                Console.WriteLine("Seu nome começa com consoante!");
+                    break;
+            }
+            
+
+        }
+    }
+}

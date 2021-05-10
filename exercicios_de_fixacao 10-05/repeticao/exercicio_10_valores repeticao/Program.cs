f16e4aca86ae90c8c13b24c67f5f4756ea33b72a
Nayara Nogueira Escudeiro do Nascimento <nanaescudeiro2015@gmail.com>
2021-05-10 18:45:42 -0300

    exercicios de condicional e repetição ok

---
+++ exercicios_de_fixacao 10-05/repeticao/exercicio_10_valores repeticao/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace exercicio_10_valores_repeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valoresUsuario = 10;
+            int[] valores = new int[valoresUsuario];
+            int menor = 0, maior = 0;
+
+            Console.WriteLine("Digite 10 números da sua preferência:");
+
+            for (var i = 0; i < valoresUsuario; i++)
+            {
+                Console.WriteLine($"Digite o {(i + 1)}° número: ");
+                valores[i] = int.Parse(Console.ReadLine());
+
+                if (i == 0)
+                {
+                    menor = valores[0];
+                    maior = valores[0];
+                }
+                if (valores[i] < menor)
+                {
+                    menor = valores[i];
+                }
+                else if (valores[i] > maior)
+                {
+                    maior = valores[i];
+                }
+            }
+                Console.WriteLine("O maior valor digitado foi: " + maior);
+                Console.WriteLine("O menor valor digitado foi: " + menor);
+        }
+    }
+}
---
+++ exercicios_de_fixacao 10-05/repeticao/exercicio_tabuada repeticao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercicio_tabuada_repeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+             Console.WriteLine($"Tabuada do {i}");
+             for (int c = 1; c <= 10; c++)
+             {
+                 Console.WriteLine($"{i}x{c} = {i*c}");
+             }
+
+            }
+        }
+    }
+}

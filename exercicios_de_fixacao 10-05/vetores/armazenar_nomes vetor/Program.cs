842863e379aa7e8fffed6901686e0e2b54d495cf
Nayara Nogueira Escudeiro do Nascimento <nanaescudeiro2015@gmail.com>
2021-05-10 21:04:33 -0300

    exercicios de fixação terminados, prestar bastante atenção na correção

---
+++ exercicios_de_fixacao 10-05/vetores/armazenar_nomes vetor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace armazenar_nomes_vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[10];
+
+            Console.WriteLine("Escreva os nomes abaixo: ");
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Digite o {i + 1}° nome: ");
+                nomes[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Qual nome você deseja procurar?: ");
+            string nomeProcurado = Console.ReadLine();
+
+            foreach (var item in nomes)
+            {
+                if (item == nomeProcurado)
+                {
+                    Console.WriteLine($"O nome {nomeProcurado} consta no sistema");
+                }
+                //else
+                //{
+                //    Console.WriteLine($"O nome {nomeProcurado} não consta no sistema");
+                //}
+
+            }
+        }
+    }
+}
---
+++ exercicios_de_fixacao 10-05/vetores/numeros_inverso vetor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace numeros_inverso_vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] valores = null;
+            valores = new int[15];
+           
+            Console.WriteLine("Digite os 15 números de sua preferência:");
+            
+             for (var i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"Digite o {(i + 1)}° número: ");
+                valores[i] = int.Parse(Console.ReadLine());
+
+            }
+             for (var i = 14; i >= 0; i--)
+             {
+                 Console.WriteLine(valores[i]);
+             }
+
+        }
+    }
+}

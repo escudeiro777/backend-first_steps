871f20236a2a81b8a57dc4133f741f7a421f92b8
Nayara Nogueira Escudeiro do Nascimento <nanaescudeiro2015@gmail.com>
2021-05-13 01:12:06 -0300

    projeto passagem aérea está com o sistema ok, mas precisa de ajuste (muitos, alias)

--- 12.05- Projeto Passagens Aéreas/Program.cs
+++ 12.05- Projeto Passagens Aéreas/Program.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 
-namespace _12._05__Projeto_Passagens_Aéreas
+namespace DesafioPassagens
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            string[] passagens = new string[5];
+            string[] nome = new string[5];
+            string[] origem = new string[5];
+            string[] destino = new string[5];
+            string[] data = new string[5];
+            bool sair = false;
+            bool voltar = false;
+            int c = 0;
+
             Console.WriteLine("Seja bem-vindo novamente à agência DeViagens!");
 
             bool validacaoCorreta = false;
@@ -21,14 +31,6 @@ namespace _12._05__Projeto_Passagens_Aéreas
 
                     //-----------------------------------------senha feito--------------------------------------------//
 
-
-                    string[] passagens = new string[4];
-                    string[] nome = new string[4];
-                    string[] origem = new string[4];
-                    string[] destino = new string[4];
-                    string[] data = new string[4];
-                    bool cadastroMenu = false;
-                    int c = 0;
                     do
                     {
                         Console.WriteLine($@"
@@ -39,71 +41,111 @@ namespace _12._05__Projeto_Passagens_Aéreas
 ======================================================================");
 
                         string opcaoUsuario = Console.ReadLine();
-
-                        switch (opcaoUsuario)
+                        do
                         {
-                            case "1":
-                                Console.WriteLine("Quantas passagens você deseja cadastrar? O número máximo de passagens que se pode cadastrar é 5.");
-                                int quantidadePassagens = int.Parse(Console.ReadLine());
+                            switch (opcaoUsuario)
+                            {
+                                case "1":
 
-                                passagens = new string[quantidadePassagens];
+                                    Console.WriteLine("Quantas passagens você deseja cadastrar? O número máximo de passagens que se pode cadastrar é 5.");
+                                    int quantidadePassagens = int.Parse(Console.ReadLine());
 
-                                for (var i = 0; i < quantidadePassagens; i++)
-                                {
-                                    if (quantidadePassagens <= 5)
+                                    passagens = new string[quantidadePassagens];
+
+                                    for (var i = 0; i < quantidadePassagens; i++)
                                     {
-                                        Console.WriteLine("Dados da " + (i + 1) + "° passagem: ");
 
-                                        Console.WriteLine("Insira seu nome completo: ");
-                                        nome[i] = Console.ReadLine();
 
-                                        Console.WriteLine("Insira seu local de origem: ");
-                                        origem[i] = Console.ReadLine();
+                                        if (quantidadePassagens <= 5)
+                                        {
+                                            Console.WriteLine("Dados da " + (i + 1) + "° passagem: ");
 
-                                        Console.WriteLine("Insira seu destino: ");
-                                        destino[i] = Console.ReadLine();
+                                            Console.WriteLine("Insira seu nome completo: ");
+                                            nome[i] = Console.ReadLine();
 
-                                        Console.WriteLine("Insira a data do seu voo: ");
-                                        data[i] = Console.ReadLine();
-                                        c++;
+                                            Console.WriteLine("Insira seu local de origem: ");
+                                            origem[i] = Console.ReadLine();
 
+                                            Console.WriteLine("Insira seu destino: ");
+                                            destino[i] = Console.ReadLine();
+
+                                            Console.WriteLine("Insira a data do seu voo: ");
+                                            data[i] = Console.ReadLine();
+
+                                            Console.WriteLine("\nDados cadastrados! Você será redirecionado para a página inicial");
+                                            voltar = true;
+                                            c++;
+
+                                        }
+                                        else if (quantidadePassagens > 5)
+                                        {
+                                            Console.WriteLine("Número limite excedido. É permitido cadastrar somente 5 passagens por vez.");
+                                            break;
+
+                                        }
                                     }
-                                    else if (quantidadePassagens > 5)
+                                    break;
+
+                                //---------------------------------------------------------------------------------------------------------------------//
+
+                                case "2":
+
+                                    Console.WriteLine("Você já cadastrou alguma passagem? (S/N)");
+                                    string respostaJunior = Console.ReadLine().ToUpper();
+
+                                    if (respostaJunior == "S")
                                     {
-                                        Console.WriteLine("Número limite excedido. É permitido cadastrar somente 5 passagens por vez.");
-                                        break;
-                                    }
-
-                                }
-
-                                break;
-
-                            case "2":
-                                Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                                Console.WriteLine($@"
+                                        Console.ForegroundColor = ConsoleColor.White;
+                                        Console.WriteLine($@"
 ============================================
 |            Dados das passagens           |
 ============================================
 ");
 
-                                for (var i = 0; i < c; i++)
-                                {
-                                    Console.WriteLine($@"
-Nome do passageiro: {nome[c]}
-Local de origem: {origem[c]}
-Destino: {destino[c]}
-Data da viagem: {data[c]}"
-                                   + "\n");
-                                }
-                                break;
-                        }
+                                        for (var i = 0; i < c; i++)
+                                        {
+                                        Console.WriteLine($@"
+Nome do passageiro: {nome[i]}
+Local de origem: {origem[i]}
+Destino: {destino[i]}
+Data da viagem: {data[i]}"
+                                           + "\n");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Primeiro você deve cadastrar seus dados na passagem!");
+                                        Console.WriteLine("Redirecionando usuário para o Menu principal");
+                                        voltar = true;
+                                    }
+                                    break;
+                                //---------------------------------------------------------------------------------------------------------------------//
+                                case "3":
 
-                    } while ( cadastroMenu == true);
-               
-                } else if (senha != "123456")
-                    {
-                        Console.WriteLine("Senha inválida! Tente novamente");
-                    }
+                                Console.WriteLine("Você tem certeza que deseja sair do sistema? (S/N)");
+                                respostaJunior = Console.ReadLine() .ToUpper();
+                                if (respostaJunior == "S")
+                                {
+                                    Console.WriteLine("Tenha uma boa viagem! Até breve...");
+                                    sair = true;
+                                } else
+                                {
+                                    Console.WriteLine("Ebaaa! Voltaremos para o início do sistema para que você consiga escolher novas opções!");
+                                    voltar = true;
+                                }
+                                    
+
+                                break;
+                            }
+
+                        } while (voltar == false);
+                    } while (sair == false);
+
+                }
+                else if (senha != "123456")
+                {
+                    Console.WriteLine("Senha inválida! Tente novamente");
+                }
             }
         }
     }
